13824b12f8a80d24802ab0a58ca0b221502733bd
nazarpysko <nazarpysko27@gmail.com>
2022-12-16 15:03:53 +0100

    unicafe warnings gone

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,12 +16,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good + neutral + bad} />
-      <StatisticLine text="average" value={(good + bad * -1) / (good + neutral + bad) || 0} />
-      <StatisticLine text="positive" value={good / (good + neutral + bad) * 100 || 0} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="average" value={(good + bad * -1) / (good + neutral + bad) || 0} />
+          <StatisticLine text="positive" value={good / (good + neutral + bad) * 100 || 0} />
+        </tbody>
+      </table>
     </div>
   )   
 }
