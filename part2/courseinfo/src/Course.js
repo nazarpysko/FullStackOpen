6f31690d78c7193259759fd2702c0456a919365f
nazarpysko <nazarpysko27@gmail.com>
2022-12-28 18:40:38 +0100

    Exercises 2.4 and 2.5 done

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,6 +1,22 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
+const Header = ({name}) => (
+    <h1>{name}</h1>
+)
+
+const Content = ({parts}) => (
+    <>
+        {parts.map( part => 
+            <Part key={part.id} part={part} />
+        )}
+    </>
+)
+
+const Part = ({part}) => (
+    <p>{part.name} {part.exercises}</p>
+)
+
+const Total = ({ parts }) => (
+    <b>total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises</b>
+)
 
 const Course = ({ course }) => (
     <div>
--- part2/courseinfo/src/Total.js
+++
@@ -1,5 +0,0 @@
-const Total = ({ parts }) => (
-    <b>total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises</b>
-)
-
-export default Total
