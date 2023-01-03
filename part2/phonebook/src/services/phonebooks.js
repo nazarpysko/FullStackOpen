2f0d0fb406563ce2dd22c1b69b8a029de3d21f90
nazarpysko <nazarpysko27@gmail.com>
2023-01-03 19:18:51 +0100

    Exercise 3.11 done

--- part2/phonebook/src/services/phonebooks.js
+++ part2/phonebook/src/services/phonebooks.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = 'http://localhost:3001/api/persons'
+const baseURL = '/api/persons' // Address for build
 // const baseURL = 'http://localhost:3001/persons'
 
 const getResponseData = (promise) => {
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,9 +5,11 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
+
 // app.use(morgan('tiny'))
-morgan.token('body', request => JSON.stringify(request.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+// morgan.token('body', request => JSON.stringify(request.body))
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
     { 
