d92330221dd40ec3b9e921d54e1cfc1012ccc74a
nazarpysko <nazarpysko27@gmail.com>
2023-01-08 20:28:59 +0100

    Exercise 3.13 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
+const dotenv = require('dotenv')
 
+dotenv.config()
+
+const express = require('express')
+const cors = require('cors')
+const Person = require('./models/person')
+
+// const morgan = require('morgan')
+// const express = require('express')
+// const cors = require('cors')
+// const Person = require('./models/person')
+
+const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
@@ -11,31 +20,33 @@ app.use(express.static('build'))
 // morgan.token('body', request => JSON.stringify(request.body))
 // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => 
+        response.json(persons)
+    )
 })
 
 app.get('/api/persons/:id', (request, response) => {
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,4 +1,6 @@
+// import mongoose from 'mongoose'
 const mongoose = require('mongoose')
+
 mongoose.set('strictQuery', false)
 
 
