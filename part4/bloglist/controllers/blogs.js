5cbe9fd41414a3e2024aaa510d328e57c996045b
nazarpysko <nazarpysko27@gmail.com>
2023-01-11 11:15:38 +0100

    Controller refactored

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,23 @@
+import Blog from '../models/blog.js'
+import express from 'express'
+let blogsRouter = express.Router()
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+export default blogsRouter
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,7 +5,8 @@ import http from 'http'
 import cors from 'cors'
 
 import mongoose from 'mongoose'
-import Blog from './models/blog.js'
+
+import blogsRouter from './controllers/blogs.js'
 
 import express from 'express'
 const app = express()
@@ -16,24 +17,7 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
