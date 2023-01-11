41080d6373e9b8a50e33ca5f441fe2febef69212
nazarpysko <nazarpysko27@gmail.com>
2023-01-11 17:42:42 +0100

    Exercise 4.5 done

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,4 +6,22 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total, blog) => total += blog.likes, 0) 
 }
 
-export { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    let favBlog = { 
+        title: '',
+        author: '',
+        likes: 0 
+    }
+
+    blogs.forEach(blog => {
+        if (blog.likes > favBlog.likes) {
+            favBlog.title = blog.title
+            favBlog.author = blog.author
+            favBlog.likes = blog.likes
+        }
+    });
+
+    return favBlog
+}
+
+export { dummy, totalLikes, favoriteBlog }
