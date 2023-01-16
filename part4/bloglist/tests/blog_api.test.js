b73f7bff279107f0ce50a0253c61c8f4567cf431
nazarpysko <nazarpysko27@gmail.com>
2023-01-16 12:52:10 +0100

    Little refactor, test bug found and solved

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -15,78 +15,83 @@ beforeEach(async () => {
     await Promise.all(promiseArray)
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-})
-
-test('correct amount of notes are returned', async () => {
-    const response = await api.get('/api/blogs')
-
-    expect(response.body).toHaveLength(initialBlogs.length)
-})
-
-test('verify id property name', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.forEach(blog => {
-        expect(blog.id).toBeDefined()
+describe('when there is initially some blogs saved', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
     })
-})
-
-test('correct amount of blogs after creating new one', async () => {
-    const newBlog = new Blog({
-        title: 'New blog test',
-        author: 'Nazar Pysko',
-        url: 'invented url',
-        likes: 42
-    })
-
-    await api.post('/api/blogs').send(newBlog)
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
-})
-
-test('correct content of the blog saved', async () => {
-    const newBlog = {
-        title: 'New blog test',
-        author: 'Nazar Pysko',
-        url: 'invented url',
-        likes: 42
-    }
-
-    const newBlogUploaded = (await api.post('/api/blogs').send(newBlog)).body
-
-    const response = await api.get('/api/blogs')
-    expect(response.body).toContainEqual(newBlogUploaded)
-})
-
-test('if likes property is missing, set it to 0', async () => {
-    const newBlog = {
-        title: 'New blog test',
-        author: 'Nazar Pysko',
-        url: 'invented url',
-    }
-
-    const newBlogUploaded = (await api.post('/api/blogs').send(newBlog)).body
-    expect(newBlogUploaded.likes).toBeDefined()
     
-    const response = await api.get('/api/blogs')
-    expect(response.body).toContainEqual(newBlogUploaded)
+    test('correct amount of notes are returned', async () => {
+        const response = await api.get('/api/blogs')
+    
+        expect(response.body).toHaveLength(initialBlogs.length)
+    })
+    
+    test('verify id property name', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.forEach(blog => {
+            expect(blog.id).toBeDefined()
+        })
+    })
 })
 
-test('if title or url are missing, respond with 400 status code', async () => {
-    const newMalformedBlog = {
-        author: 'Nazar Pysko',
-        url: 'invented url'
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newMalformedBlog)
-        .expect(400)
+describe('when adding new note', () => {
+    test('correct amount of blogs after creating new one', async () => {
+        const newBlog = {
+            title: 'New blog test',
+            author: 'Nazar Pysko',
+            url: 'invented url',
+            likes: 42
+        }
+    
+        await api.post('/api/blogs').send(newBlog)
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+    })
+    
+    test('correct content of the blog saved', async () => {
+        const newBlog = {
+            title: 'New blog test',
+            author: 'Nazar Pysko',
+            url: 'invented url',
+            likes: 42
+        }
+    
+        const newBlogUploaded = (await api.post('/api/blogs').send(newBlog)).body
+    
+        const response = await api.get('/api/blogs')
+        expect(response.body).toContainEqual(newBlogUploaded)
+    })
+    
+    test('if likes property is missing, set it to 0', async () => {
+        const newBlog = {
+            title: 'New blog test',
+            author: 'Nazar Pysko',
+            url: 'invented url',
+        }
+    
+        const newBlogUploaded = (await api.post('/api/blogs').send(newBlog)).body
+        expect(newBlogUploaded.likes).toBeDefined()
+        
+        const response = await api.get('/api/blogs')
+        expect(response.body).toContainEqual(newBlogUploaded)
+    })
+    
+    test('if title or url are missing, respond with 400 status code', async () => {
+        const newMalformedBlog = {
+            author: 'Nazar Pysko',
+            url: 'invented url'
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newMalformedBlog)
+            .expect(400)
+    })    
 })
+
 
 afterAll(() => {
     mongoose.connection.close()
