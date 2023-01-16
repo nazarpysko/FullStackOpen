cdabfe9587c7e1939972eeff53a769fcd9780988
nazarpysko <nazarpysko27@gmail.com>
2023-01-16 10:25:14 +0100

    Exercise 4.9 done

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -28,6 +28,13 @@ test('correct amount of notes are returned', async () => {
     expect(response.body).toHaveLength(initialBlogs.length)
 })
 
+test('verify id property name', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+    })
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
