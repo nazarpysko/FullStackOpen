ca67c5246d25f9db5f7268643c5ab389c40fd476
nazarpysko <nazarpysko27@gmail.com>
2023-02-13 12:19:48 +0100

    Exercise 5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Displays basic info about blog', () => {
@@ -25,5 +26,30 @@ test('Displays basic info about blog', () => {
 
   const blogInfo = container.querySelector('.info')
   expect(blogInfo).toHaveStyle('display: none')
+})
 
+test('Blog\'s URL and number of likes are shown when view button is clicked', async () => {
+  const blog = {
+    title: 'The Go libraries that never failed us: 22 libraries you need to know',
+    author: 'Robert Laszczak',
+    url: 'https://threedots.tech/post/list-of-recommended-libraries/',
+    likes: 42,
+    user: {
+      username: 'eduardo'
+    }
+  }
+
+  const user = {
+    username: 'a'
+  }
+
+  const userInteraction = userEvent.setup()
+  const { container } = render(<Blog blog={blog} user={user}/>)
+  const viewButton = screen.getByText('view')
+  await userInteraction.click(viewButton)
+
+  const info = container.querySelector('.info')
+  expect(info).toHaveStyle('display: block')
+  const buttonLabel = screen.getByText('hide')
+  expect(buttonLabel).toBeDefined()
 })
