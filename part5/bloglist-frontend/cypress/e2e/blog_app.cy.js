8d0e1f8ea39cb26e0ad082c5d6640373b83ebbdf
nazarpysko <nazarpysko27@gmail.com>
2023-02-14 10:57:56 +0100

    Exercise 5.18 done

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -20,4 +20,22 @@ describe('Blog app', function() {
     cy.get('#password').should('exist')
     cy.contains('button', 'login').should('exist')
   })
+
+  describe('Login', () => {
+    it.only('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in').should('exist')
+    })
+
+    it.only('fails with invalid credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrongPassword')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
 })
--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -114,7 +114,7 @@ const App = () => {
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
-          <button type='submit'> login </button>
+          <button id='login-button' type='submit'> login </button>
         </form>
       </div>
     )
