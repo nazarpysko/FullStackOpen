f14a4f0a59b532540cc68de372ea70411f2d8ad9
nazarpysko <nazarpysko27@gmail.com>
2023-02-14 11:13:21 +0100

    Exercise 5.19 done

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -22,7 +22,7 @@ describe('Blog app', function() {
   })
 
   describe('Login', () => {
-    it.only('succeeds with correct credentials', function() {
+    it('succeeds with correct credentials', function() {
       cy.get('#username').type('mluukkai')
       cy.get('#password').type('salainen')
       cy.get('#login-button').click()
@@ -30,12 +30,29 @@ describe('Blog app', function() {
       cy.contains('Matti Luukkainen logged in').should('exist')
     })
 
-    it.only('fails with invalid credentials', function() {
+    it('fails with invalid credentials', function() {
       cy.get('#username').type('mluukkai')
       cy.get('#password').type('wrongPassword')
       cy.get('#login-button').click()
 
       cy.contains('wrong username or password').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('button', 'new blog').click()
+      cy.get('#blog-title-input').type('The Go libraries that never failed us: 22 libraries you need to know')
+      cy.get('#blog-author-input').type('Robert Laszczak')
+      cy.get('#blog-url-input').type('https://threedots.tech/post/list-of-recommended-libraries/')
+      cy.contains('button', 'create').click()
+
+      cy.contains('The Go libraries that never failed us: 22 libraries you need to know').should('exist')
+      cy.contains('Robert Laszczak').should('exist')
     })
   })
 })
