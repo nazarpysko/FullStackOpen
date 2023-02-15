6ace3e48f487d24af408fc05c85f3fab6355fb02
nazarpysko <nazarpysko27@gmail.com>
2023-02-15 12:11:53 +0100

    Exercise 6.1 done

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -11,9 +11,9 @@ const counterReducer = (state = initialState, action) => {
     case 'GOOD':
       return { ...state, good: state.good + 1 }
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
-      return state
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
       return state
     default: return state
