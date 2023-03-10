6e520f818417707a736d04c3b72ef6b5df1355ba
nazarpysko <nazarpysko27@gmail.com>
2023-03-10 10:27:22 +0100

    Exercise 7.5 done

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -88,6 +88,12 @@ const CreateNew = props => {
     navigate('/')
   };
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -105,6 +111,7 @@ const CreateNew = props => {
           <input { ...info } />
         </div>
         <button>create</button>
+        <button type="reset" onClick={() => handleReset()}>reset</button>
       </form>
     </div>
   );
